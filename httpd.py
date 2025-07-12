5edfb7a3ceeb388a65cc8b7e9b66e55e53afc13e
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2025-07-13 00:28:19 +0900

    show images

--- httpd.py
+++ httpd.py
@@ -6,6 +6,10 @@ from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 import base64
 import argparse
 import socket
+from mimetypes import guess_type
+import urllib.parse
+import html
+import io
 
 
 class Handler(SimpleHTTPRequestHandler):
@@ -56,6 +60,94 @@ class Handler(SimpleHTTPRequestHandler):
         if not self._authorized():
             return
         super(Handler, self).do_GET()
+
+    def list_directory(self, path):
+        try:
+            contents = os.listdir(path)
+        except OSError:
+            self.send_error(403)
+            return None
+
+        contents = list(filter(lambda x: x and x[0] != ".", contents))
+        contents.sort(key=lambda a: a.lower())
+
+        def is_image(name: str):
+            return guess_type(name)[0].startswith("image/")
+
+        dirs = list(filter(lambda x: os.path.isdir(os.path.join(path, x)), contents))
+        files = list(filter(lambda x: x not in dirs, contents))
+        images = list(filter(is_image, files))
+
+        r = []
+        try:
+            displaypath = urllib.parse.unquote(self.path, errors="surrogatepass")
+        except UnicodeDecodeError:
+            displaypath = urllib.parse.unquote(self.path)
+        displaypath = html.escape(displaypath, quote=False)
+        enc = sys.getfilesystemencoding()
+        title = displaypath
+        r.append("<!DOCTYPE HTML>")
+        r.append('<html lang="en">')
+        r.append("<head>")
+        r.append(f'<meta charset="{enc}">')
+        r.append("<style>")
+        r.append("a { text-decoration: none; }")  # disable underline for links
+        r.append("img { max-width: 250px; max-height:250px }")
+        r.append("</style>")
+
+        r.append(f"<title>{title}</title>\n</head>")
+        r.append("<body>\n")
+
+        def make_list(contents: list[str]):
+            if not contents:
+                return
+
+            r.append("<ul>")
+            for name in contents:
+                fullname = os.path.join(path, name)
+                displayname = linkname = name
+                # Append / for directories or @ for symbolic links
+                if os.path.isdir(fullname):
+                    displayname = name + "/"
+                    linkname = name + "/"
+                if os.path.islink(fullname):
+                    displayname = name + "@"
+                    # Note: a link to a directory displays with @ and links with /
+                r.append(
+                    '<li><a href="%s">%s</a></li>'
+                    % (
+                        urllib.parse.quote(linkname, errors="surrogatepass"),
+                        html.escape(displayname, quote=False),
+                    )
+                )
+            r.append("</ul><hr>\n")
+
+        make_list(dirs)
+        make_list(files)
+
+        image_limit = 100
+        for i in images[:image_limit]:
+            r.append(
+                '<a href="%s"><img src="%s" alt="%s"></a>\n'
+                % (
+                    urllib.parse.quote(i, errors="surrogatepass"),
+                    urllib.parse.quote(i, errors="surrogatepass"),
+                    html.escape(i, quote=False),
+                )
+            )
+        if len(images) > image_limit:
+            r.append(f"<br> only {image_limit} of {len(images)} images are shown .")
+
+        r.append("</body>\n</html>\n")
+        encoded = "\n".join(r).encode(enc, "surrogateescape")
+        f = io.BytesIO()
+        f.write(encoded)
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=%s" % enc)
+        self.send_header("Content-Length", str(len(encoded)))
+        self.end_headers()
+        return f
 
 
 def daemonize(logfile: str | Path = "/dev/null"):
